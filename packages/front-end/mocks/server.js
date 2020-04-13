6b7cde87eaa729e7da194400cf5573b0481c808c
yardz <wadge.motta@gmail.com>
2020-04-13 16:00:17 -0300

    setup mock

--- packages/front-end/mocks/server.js
+++ packages/front-end/mocks/server.js
@@ -1,28 +1,51 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('mocks/db.json')
-const middlewares = jsonServer.defaults()
-const db = router.db
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('mocks/db.json');
+const middlewares = jsonServer.defaults();
+const db = router.db;
 
-const port = 3100;
+const port = 3001;
 
-server.use(middlewares)
+server.use(middlewares);
 
-server.get('/ocasioes', (req, res) => {
-  res
-    .status(200)
-    .json(db.get('ocasioes').map(ocasiao => ({ 'nome': ocasiao.nome, 'id': ocasiao.id, })).value());
-})
+// get lista
+server.get('/atividades', (req, res) => {
+  res.status(200).json(
+    db
+      .get('atividades')
+      .map(({ id }) => id)
+      .value(),
+  );
+});
 
-server.get('/ocasioes/:id', (req, res) => {
-  res
-    .status(200)
-    .json(db.get('ocasioes').filter({ id: req.params.id }).value());
-})
+// get unico
+server.get('/atividades/:id', (req, res) => {
+  res.status(200).json(
+    db
+      .get('atividades')
+      .filter({ id: Number(req.params.id) })
+      .value(),
+  );
+});
 
-server.use(router)
+// criar
+server.post('/atividades', (req, res) => {
+  res.status(201);
+});
 
+// editar
+server.put('/atividades/:id', (req, res) => {
+  res.status(201);
+});
+
+// deletar
+server.delete('/atividades/:id', (req, res) => {
+  res.status(200).json({ mensagem: "Atividade apagada com sucesso!" });
+});
+
+
+server.use(router);
 
 server.listen(port, () => {
-  console.log(`Server start at port: ${port}`)
-})
+  console.log(`Server start at port: ${port}`);
+});
