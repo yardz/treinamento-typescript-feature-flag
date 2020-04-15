f5ee9a70bb122136cd400754c7d680c0f2ffa3f8
yardz <wadge.motta@gmail.com>
2020-04-15 11:47:13 -0300

    Config de teste unitário

---
+++ packages/back-end/src/main/server/services/gateway/consultar-atividades/consultar-atividades.gateway.spec.ts
@@ -0,0 +1,11 @@
+//import { consultarAtividadesGateway } from './consultar-atividades.gateway';
+
+describe('Teste unitário "consultarAtividadesGateway"', () => {
+	beforeEach(() => {
+		jest.resetAllMocks();
+	});
+
+	it('deve disparar fazer signOut do aluno quando chamada de api retorna status 403', () => {
+		expect(true).toBe(true);
+	});
+});
